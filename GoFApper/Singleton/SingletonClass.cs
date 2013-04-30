a5c4be05356ebd39fe7d6cc9dfee3e3fb1a15718
Joshua Zhao <shuozhaous@gmail.com>
2013-04-30 00:03:58 -0700

    Gang of Four Start

---
+++ GoFApper/Singleton/SingletonClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFApper.Singleton
+{
+    public class SingletonClass
+    {
+        static readonly SingletonClass staticIns = new SingletonClass();
+        Int32 initialTimes = 0;
+
+        public static SingletonClass Instance
+        {
+            get
+            {
+                return staticIns;
+            }
+        }
+
+        public SingletonClass()
+        {
+            initialTimes++;
+        }
+
+        public Int32 InstaceCount
+        {
+            get
+            {
+                return initialTimes;
+            }
+        }
+    }
+}
